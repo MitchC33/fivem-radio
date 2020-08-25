5b5203d9c838b4bda264a331890187fab1ec3672
MitchC33 <69934680+MitchC33@users.noreply.github.com>
2020-08-25 13:30:18 +0100

    Update __resource.lua
    Some radio stations there which will be good for your server.

--- radio/__resource.lua
+++ radio/__resource.lua
@@ -1,9 +1,12 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_02_POP" { url = "https://revolutionradio.ru:8443/live.ogg", volume = 0.2 }
-supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
-
+supersede_radio "[Mirror Park]" { url = "[https://planetradio.co.uk/kisstory/]", volume = 0.4, name = "[Kisstory]" }
+supersede_radio "[Los Santo Rock Radio]" { url = "[https://simulatorhits.com/]", volume = 0.4, name = "[Simulator Hits]" }
+supersede_radio "[West Coast Classics]" { url = "[https://www.globalplayer.com/live/capital/uk/]", volume = 0.4, name = "[Capital FM]" }
+supersede_radio "[Non-Stop-Pop FM]" { url = "[https://keyfm.net/splash/Key.Home]", volume = 0.4, name = "[Key FM]" }
+supersede_radio "[Rebel Radio]" { url = "[https://dual.pw/]", volume = 0.4, name = "[Dual]" }
+supersede_radio "[The Lab]" { url = "[https://repeatradio.net/]", volume = 0.4, name = "[Repeat Radio]" }
 files {
 	"index.html"
 }
